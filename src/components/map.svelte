75ab7efd04cee8e9b0051754cfb6f8975cc8cb93
Website Admin <coordinator@fullmoonprod.com>
2021-12-03 12:31:13 -0600

    adjust map

--- src/components/map.svelte
+++ src/components/map.svelte
@@ -3,7 +3,7 @@
 
 <iframe
     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3096.2078345928967!2d-94.60266478464358!3d39.10173817953963!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87c0f054432e9f09%3A0xbd9828d8846f2708!2s1308%20W%2011th%20St%2C%20Kansas%20City%2C%20MO%2064101!5e0!3m2!1sen!2sus!4v1638472142090!5m2!1sen!2sus"
-    width="350"
+    width="300"
     title="location"
     height="300"
     allowfullscreen=""
